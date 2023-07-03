fb88c9dc832e766d320baad47dedaef822291949
Adrian Jose Diaz Guilamo <ajdiaz@ADRIANDIAZ>
2023-07-03 11:22:51 -0400

    stable version (prod)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,21 +1,21 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.AsistenciaVialMiembros.app',
-  appName: 'AsistenciaVialMiembros',
-  webDir: 'www',
-  bundledWebRuntime: false,
-  cordova: {
-    preferences: {
-      ScrollEnabled: 'false',
-      BackupWebStorage: 'none',
-      SplashMaintainAspectRatio: 'true',
-      FadeSplashScreenDuration: '300',
-      SplashShowOnlyFirstTime: 'false',
-      SplashScreen: 'screen',
-      SplashScreenDelay: '3000'
-    }
-  }
+	appId: 'com.ReportarAsistencia.app',
+	appName: 'Reportar Asistencia',
+	webDir: 'www',
+	bundledWebRuntime: false,
+	cordova: {
+		preferences: {
+			ScrollEnabled: 'false',
+			BackupWebStorage: 'none',
+			SplashMaintainAspectRatio: 'true',
+			FadeSplashScreenDuration: '300',
+			SplashShowOnlyFirstTime: 'false',
+			SplashScreen: 'screen',
+			SplashScreenDelay: '3000',
+		},
+	},
 };
 
 export default config;
