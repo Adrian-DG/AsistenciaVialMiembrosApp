e2da2777a7a9275f3e6be51c1ab9b6ad890081bb
Adrian Jose Diaz Guilamo <ajdiaz@ADRIANDIAZ>
2023-10-19 15:35:39 -0400

    minor changes on EditForm

--- src/app/modules/dashboard/pages/edit/edit.component.ts
+++ src/app/modules/dashboard/pages/edit/edit.component.ts
@@ -5,20 +5,37 @@ import { IAsistenciaEditViewModel } from '../../interfaces/iasistencia-edit-view
 import { FormControl, FormGroup } from '@angular/forms';
 import { CacheService } from 'src/app/modules/cache/services/cache.service';
 
+import { ComponentCanDeactivate } from 'src/app/guard/leave.guard';
+import {
+	ProvinciasArray,
+	VehicleColors,
+	VehicleTypesArray,
+} from '../../constants/app.const';
+
 @Component({
 	selector: 'AsistenciaVial-edit',
 	templateUrl: './edit.component.html',
 	styleUrls: ['./edit.component.scss'],
 })
-export class EditComponent implements OnInit, AfterViewInit {
+export class EditComponent
+	implements OnInit, AfterViewInit, ComponentCanDeactivate
+{
 	id!: number;
 	asistenciaObject: IAsistenciaEditViewModel | undefined;
+
+	provinciasArray = ProvinciasArray;
+	coloresVehiculosArray = VehicleColors;
+	tipoVehiculosArray = VehicleTypesArray;
 
 	constructor(
 		private $activeRoute: ActivatedRoute,
 		private _asistencias: AsistanceService,
 		public _cache: CacheService
 	) {}
+
+	canDeactivate(): boolean {
+		return false;
+	}
 
 	ngOnInit() {
 		this.$activeRoute.params.subscribe((params) => {
