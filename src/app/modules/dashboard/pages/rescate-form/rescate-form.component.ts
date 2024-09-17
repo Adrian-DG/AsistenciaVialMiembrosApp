ab88a56f6466ac3cced136b1d3a68a2cf4f8592b
Adrian-DG <adrian96guilamo@gmail.com>
2024-09-16 23:36:43 -0400

    formulario de rescate in progress

--- src/app/modules/dashboard/pages/rescate-form/rescate-form.component.ts
+++ src/app/modules/dashboard/pages/rescate-form/rescate-form.component.ts
@@ -7,7 +7,12 @@ import {
 import { CacheService } from 'src/app/modules/cache/services/cache.service';
 import { AsistenciaRescateService } from '../../services/rescate/asistencia-rescate.service';
 
-import { ProvinciasArray } from '../../constants/app.const';
+import {
+	ProvinciasArray,
+	VehicleColors,
+	VehicleTypesArray,
+} from '../../constants/app.const';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
 	selector: 'ReportarAsistencia-rescate-form',
@@ -17,10 +22,13 @@ import { ProvinciasArray } from '../../constants/app.const';
 export class RescateFormComponent implements OnInit {
 	constructor(
 		public _cache: CacheService,
-		private _rescate: AsistenciaRescateService
+		private _rescate: AsistenciaRescateService,
+		private $fb: FormBuilder
 	) {}
 
 	proviciasList = ProvinciasArray;
+	coloresList = VehicleColors;
+	tiposList = VehicleTypesArray;
 
 	rescateObj: IAsistenciaRescateCreate = {
 		tipoDocumento: 0,
@@ -36,7 +44,7 @@ export class RescateFormComponent implements OnInit {
 		condicionVictima: 0,
 		cantidadPersonasFallecidas: 0,
 		cantidadPersonasLesionadas: 0,
-		cantidadVehiculos: 0,
+		cantidadVehiculos: 1,
 		detalles: '',
 		entidadRecibioPertenencias: 0,
 		entidadRecibioVictima: 0,
@@ -47,10 +55,50 @@ export class RescateFormComponent implements OnInit {
 		maniobrasAplicadas: [],
 	};
 
-	ngOnInit() {}
+	vehiculosInvolucradosForm = this.$fb.group({
+		vehiculosArray: this.$fb.array([
+			new FormGroup({
+				vehiculoTipoId: new FormControl(0),
+				vehiculoColorId: new FormControl(0),
+				vehiculoMarcaId: new FormControl(0),
+				marcaTxt: new FormControl(''),
+				vehiculoModeloId: new FormControl(0),
+				modeloTxt: new FormControl(''),
+				placa: new FormControl(''),
+			}),
+		]),
+	});
+
+	ngOnInit() {
+		this._cache.getResource('VehiculoMarca');
+		this._cache.getResource('VehiculoMarca');
+	}
+
+	get isFormValid() {
+		const { provinciaId, municipioId } = this.rescateObj;
+		return [provinciaId, municipioId].every((value) => value > 0);
+	}
+
+	addVehicle() {
+		this.vehiculosInvolucradosForm.controls.vehiculosArray.push(
+			new FormGroup({
+				vehiculoTipoId: new FormControl(0),
+				vehiculoColorId: new FormControl(0),
+				vehiculoMarcaId: new FormControl(0),
+				marcaTxt: new FormControl(''),
+				vehiculoModeloId: new FormControl(0),
+				modeloTxt: new FormControl(''),
+				placa: new FormControl(''),
+			})
+		);
+
+		this.rescateObj.cantidadVehiculos += 1;
+	}
 
 	async createAsistance(): Promise<void> {
 		this.rescateObj.unidadMiembroId = await this._rescate.getUnitMemberId();
+		this.rescateObj.vehiculosInvolucrados = this.vehiculosInvolucradosForm
+			.controls.vehiculosArray.value as IVehiculosInvolucrados[];
 		this._rescate.createAsistenciaRescate(this.rescateObj);
 	}
 }
